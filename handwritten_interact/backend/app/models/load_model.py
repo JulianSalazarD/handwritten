479f29fad4aaa89afeb4e3a2384978a198cb5a17
JulianSalazarD <juliansal24@gmail.com>
2024-09-27 21:55:28 -0500

    add READ ME

--- handwritten_interact/backend/app/models/load_model.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.keras.models import load_model
-
-def load_ml_model():
-    # Load the model
-    model = load_model('backend/app/models/handwritten_recognition.h5')
-    return model
